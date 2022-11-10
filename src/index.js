e7207465406468290d93208fd7b971872541666e
Elena Pastushenko <heleora_net@ukr.net>
2022-11-10 18:34:52 +0200

    start using modules

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
-// На практиці взагалі не використовуються запити типу GET https://bookstore.com/api/orders, лише через fetch()?
-// if (!response.ok){} -> означає dbrjyfnb при будь-якій відповіді відмінній від 200? Навіть якщо будуть інші успішні відповіді 201, 202 тощо?
+const inputRef = document.querySelector("#search-box");
+const name = inputRef.elements.name.value;
+console.log(name);
 
-// https://restcountries.com/
+inputRef.addEventListener("input", fetchCountries);
 
+function fetchCountries(name) {
+fetch(`
+    https://restcountries.com/v3.1/name/{name}`
+)
+}
