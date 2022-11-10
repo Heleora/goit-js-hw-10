4a7773d6a53195b0edfe1ea27e0da2d577998b90
Elena Pastushenko <heleora_net@ukr.net>
2022-11-10 18:15:23 +0200

    Creates project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+
+// На практиці взагалі не використовуються запити типу GET https://bookstore.com/api/orders, лише через fetch()?
+// if (!response.ok){} -> означає dbrjyfnb при будь-якій відповіді відмінній від 200? Навіть якщо будуть інші успішні відповіді 201, 202 тощо?
+
+// https://restcountries.com/
+
