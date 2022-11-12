1c59740654272fd01617c48c70735e0df6e58ced
Elena Pastushenko <heleora_net@ukr.net>
2022-11-12 20:55:39 +0200

    catches errors

--- src/index.js
+++ src/index.js
@@ -69,8 +69,13 @@ function fetchCountries(evt) {
     
     fetch(`
     https://restcountries.com/v3.1/name/${searchRequestByCountryName}?fields=name,capital,population,flags,languages`)
-    .then(response => response.json())
-.then(data => {
+    .then(response => {
+      if(!response.ok) {
+        throw new Error (response.status);
+      }
+      return response.json()
+    })
+    .then(data => {
     console.log(data);
 
     if (data.length > 10) {
@@ -83,6 +88,9 @@ function fetchCountries(evt) {
     }
     
     outputListOfCountries(data);    
+})
+.catch(() => {
+  Notiflix.Notify.failure("Oops, there is no country with that name");
 })
 };
 
